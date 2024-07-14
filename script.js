4badb98158e64af8142b44c0ad072fe03a3483fd
Ujjwal Sonawat <146330690+ujjwal-sona77@users.noreply.github.com>
2024-07-14 21:18:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ Shery.mouseFollower({
 
 Shery.textAnimate("#heading" /* Element to target.*/, {
   //Parameters are optional.
+  style: 2,
+  y: 10,
+  delay: 0.1,
+  duration: 2,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
+});
+
+Shery.textAnimate("#centerh1" /* Element to target.*/, {
+  //Parameters are optional.
   style: 1,
   y: 10,
   delay: 0.1,
